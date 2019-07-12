47d0157bcfd44d6f9dcb9ba66ca3986607b0294b
Fiodar Morau <chelovekdrakon@gmail.com>
2019-07-12 22:27:47 +0300

    project init

---
+++ FishDay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FishDay
+//
+//  Created by Фёдор Морев on 7/12/19.
+//  Copyright © 2019 Фёдор Морев. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FishDay/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  FishDay
+//
+//  Created by Фёдор Морев on 7/12/19.
+//  Copyright © 2019 Фёдор Морев. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
